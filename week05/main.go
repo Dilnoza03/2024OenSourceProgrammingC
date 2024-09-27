06d01338a2c319c14c2fbb892ba90014d71001b1
dilnozaakhmedzhanova7 <dilnozaakhmedzhanova7@gmail.com>
2024-09-27 15:10:15 +0600

    v0.2

---
+++ week05/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	var f float32 = 3.99
+
+	i := 55
+	fmt.Println(f, math.Ceil(3.49))
+	fmt.Println(strings.Title("dika"))
+	fmt.Println("i %d\n", i)
+	fmt.Println("i ", i, "\n")
+	fmt.Println("i ", i)
+
+}
