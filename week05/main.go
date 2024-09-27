537a35b899e9f8910c7001c1db2ab72d7026a072
dilnozaakhmedzhanova7 <dilnozaakhmedzhanova7@gmail.com>
2024-09-27 15:18:18 +0600

    vo.3 Type

--- week05/main.go
+++ week05/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
 func main() {
-	var f float32 = 3.99
+	// var f float32 = 3.99
 
 	i := 55
+	f := 3.99
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+
 	fmt.Println(f, math.Ceil(3.49))
 	fmt.Println(strings.Title("dika"))
-	fmt.Println("i %d\n", i)
-	fmt.Println("i ", i, "\n")
+	fmt.Println("i %s\n", i)
+	fmt.Println("i", i, "\n")
 	fmt.Println("i ", i)
 
 }
