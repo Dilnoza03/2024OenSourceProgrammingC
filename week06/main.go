6b86effe6544a7e7a1a86091edc06aad948db129
dilnozaakhmedzhanova7 <dilnozaakhmedzhanova7@gmail.com>
2024-10-04 14:40:49 +0600

    v0.5 Convertation

--- week06/main.go
+++ week06/main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var i int = 55
-	f := 12.9
-	fmt.Println("value i :%d,value f:%f\n", i, f)
-	//fmt.Println("%d*%f=%f/n", i, f, i*f)
-	fmt.Println("%d*%f=%f/n", i, f, float64(i)*f)
-	fmt.Println(reflect.TypeOf(f))
+	i := 55              //var i int = 55
+	var f float64 = 12.9 //f := 12.9
+	// fmt.Printf("value i :%d,value f:%f\n", i, f)
+	//fmt.Printf("%d*%f=%f/n", i, f, i*f)
+	fmt.Printf("%d*%f=%f\n", i, f, i*int(f))
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 
 }
