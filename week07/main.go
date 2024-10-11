ee28f52384ab8317aa3c7350f502cb43fe450f0b
dilnozaakhmedzhanova7 <dilnozaakhmedzhanova7@gmail.com>
2024-10-11 15:28:12 +0600

    v1.3 Parse

--- week07/main.go
+++ week07/main.go
@@ -1,3 +1,24 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+// )
+
+// func main() {
+
+// 	// var now time.Time = time.Now()
+// 	// //var month time.Month = now.Month()
+// 	// fmt.Printf("Now %dyear %dmonth %d day \n", now.Year(), int(now.Month()), now.Day())
+// 	// fmt.Printf("Now %dhour %dminute %dsecond ", now.Hour(), now.Minute(), now.Second())
+
+// 	var army string = "오늘은 $가와 $만에 중서을 다하는 대한민 $육군임니다"
+// 	armyFixed := strings.NewReplacer("$", "국")
+// 	fmt.Println(army)
+// 	fmt.Println(armyFixed.Replace(army))
+
+// }
+
 package main
 
 import (
@@ -5,32 +26,38 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
-	// var now time.Time = time.Now()
-	// //var month time.Month = now.Month()
-	// fmt.Printf("Now %dyear %dmonth %d day \n", now.Year(), int(now.Month()), now.Day())
-	// fmt.Printf("Now %dhour %dminute %dsecond ", now.Hour(), now.Minute(), now.Second())
-
-	// var army string = "오늘은 $가와 $만에 중서을 다하는 대한민 $육군임니다"
-	// armyFixed := strings.NewReplacer("$", "국")
-	// fmt.Println(army)
-	// fmt.Println(armyFixed.Replace(army))
-
-	// var army string = "오늘은 $가와 $만에 중서을 다하는 대한민 $육군임니다"
-	// armyFixed := strings.NewReplacer("$", "국")
-	// fmt.Println(army)
-	// fmt.Println(armyFixed.Replace(army))
-
 	in := bufio.NewReader(os.Stdin)
-	fmt.Println("Input your name:")
-	name, err := in.ReadString('\n')
+	fmt.Println("Input your score:")
+	i, err := in.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)
+	score, _ := strconv.ParseInt(i, 10, 32)
+	if score >= 60 {
+		fmt.Println("A")
+		fmt.Printf("%d\n", score)
 	} else {
-		fmt.Println(name)
+		fmt.Println("BCDF")
+		fmt.Printf("%d\n", score)
 	}
 
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var int int = 99
+// 	a := 1
+// 	fmt. Println(int + a)
+// }
