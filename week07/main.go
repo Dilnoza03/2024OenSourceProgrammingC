bdef736573bbc1254f3af9a6dc8f90159a893140
dilnozaakhmedzhanova7 <dilnozaakhmedzhanova7@gmail.com>
2024-10-11 14:29:26 +0600

    v0.11 Stdin

--- week07/main.go
+++ week07/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
 func main() {
@@ -12,9 +13,20 @@ func main() {
 	// fmt.Printf("Now %dyear %dmonth %d day \n", now.Year(), int(now.Month()), now.Day())
 	// fmt.Printf("Now %dhour %dminute %dsecond ", now.Hour(), now.Minute(), now.Second())
 
-	var army string = "오늘은 $가와 $만에 중서을 다하는 대한민 $육군임니다"
-	armyFixed := strings.NewReplacer("$", "국")
-	fmt.Println(army)
-	fmt.Println(armyFixed.Replace(army))
+	// var army string = "오늘은 $가와 $만에 중서을 다하는 대한민 $육군임니다"
+	// armyFixed := strings.NewReplacer("$", "국")
+	// fmt.Println(army)
+	// fmt.Println(armyFixed.Replace(army))
+
+	// var army string = "오늘은 $가와 $만에 중서을 다하는 대한민 $육군임니다"
+	// armyFixed := strings.NewReplacer("$", "국")
+	// fmt.Println(army)
+	// fmt.Println(armyFixed.Replace(army))
+
+	in := bufio.NewReader(os.Stdin)
+	fmt.Println("Input your name:")
+	name, err := in.ReadString('\n')
+	fmt.Println(name)
+	fmt.Println(err)
 
 }
