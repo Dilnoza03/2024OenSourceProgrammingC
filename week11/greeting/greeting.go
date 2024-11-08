7ac32967ce0d9dfeb7d0c52c6a8d0d61c168d0f4
dilnozaakhmedzhanova7 <dilnozaakhmedzhanova7@gmail.com>
2024-11-08 15:14:36 +0600

    v3.5 Package

--- week11/greeting/greeting.go
+++ week11/greeting/greeting.go
@@ -2,10 +2,18 @@ package greeting
 
 import "fmt"
 
+// Функция для печати приветствия "Hi"
 func Hi(name string) {
 	fmt.Printf("Hi %s!\n", name)
 }
 
+// Функция для печати приветствия "Hello"
 func Hello(name string) {
 	fmt.Printf("Hello %s~\n", name)
+}
+
+// Функция для вызова приветствий на английском
+func EnglishGreetings(name string) {
+	Hello(name)
+	Hi(name)
 }
