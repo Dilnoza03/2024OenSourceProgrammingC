9ab94bfde0c8edd7bfc797ce4db1bc93fdeec30a
dilnozaakhmedzhanova7 <dilnozaakhmedzhanova7@gmail.com>
2024-11-15 14:29:27 +0600

    v3.8 Array

--- week12/main.go
+++ week12/main.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
-	// var scores [3]int
-	// scores[1] = 90
+	var scores [3]int
+	scores[1] = 90
 	// fmt.Println(scores[1], scores[0]) //, scores[3])
-	// fmt.Printf("%v\n", scores)
+	// fmt.Printf("%#v\n", scores)
+
+	// for i := 0; i <= len(scores); i++ {
+	for i := 0; i < len(scores); i++ {
+		fmt.Printf("%d", scores[i])
+	}
 
 	// var dates [3]time.Time
 	// dates[1] = time.Unix(194720001, 0)
@@ -29,8 +34,12 @@ func main() {
 		time.Unix(0, 0),
 		time.Unix(1, 0),
 		time.Unix(194720001, 0)}
-	fmt.Println(dates[0], dates[1], dates[2])
-	fmt.Printf("%#v\n", dates)
-	fmt.Println(dates)
+	// fmt.Println(dates[0], dates[1], dates[2])
+	// fmt.Printf("%#v\n", dates)
+	// fmt.Println(dates)
+	fmt.Println()
+	for _, date := range dates { // like python dtyle, SAFE!
+		fmt.Println(date)
+	}
 
 }
