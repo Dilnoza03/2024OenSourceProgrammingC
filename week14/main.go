fdc759577acf58b808e11775a4e0491216eabd86
dilnozaakhmedzhanova7 <dilnozaakhmedzhanova7@gmail.com>
2024-11-29 14:50:19 +0600

    v4.8 Struct

--- week14/main.go
+++ week14/main.go
@@ -4,23 +4,22 @@ import (
 	"fmt"
 )
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student1 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+
+	var student1 student
 	student1.id = 202388007
 	student1.name = "Dilnoza"
 	student1.gpa = 4.5
 	fmt.Println(student1.gpa)
 
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
-	student2.id = 202388007
+	var student2 student
+	student2.id = 202444990
 	student2.name = "Aidana"
 	student2.gpa = 4.43
 	fmt.Println(student2.id)
